a35c1c80d8d00ce730b43bc998f749b901db2802
Markus Schlueter <mschlueter@geomar.de>
2021-04-10 04:38:45 +0200

    Add launch script

---
+++ LirenaCapture/scripts/launchLirenaCapture.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# launch script / snippets with common param combinations
+
+# launch MagewellEco without GUI, but with local show&dump
+GST_DEBUG=3,appsrc:6 \
+__GL_SYNC_TO_VBLANK=0 \
+./builddir/lirenaCapture \
+    192.168.1.169 5001 \
+    -d MagewellEco \
+    --localdisplay \
+    -o mydump.mpg \
+    
+exit 0
+
+
+
+
+# snippet to test CLI param parsing:
+GST_DEBUG=3,appsrc:6 \
+__GL_SYNC_TO_VBLANK=0 \
+./builddir/lirenaCapture \
+    192.168.1.169 5001 \
+    -d MagewellEco \
+    -x 4096 -y 2160 \
+    -f 24 \
+    --localdisplay \
+    --localGUI \
+    -c \
+    -e 25 \
+    -o mydump.mpg \
+    -t \
+    
+exit 0
