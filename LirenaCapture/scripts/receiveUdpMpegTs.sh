3a973345498e987688f961f12bf13841a70fd6e4
Markus Schlüter <mschlueter@geomar.de>
2021-04-16 23:55:22 +0200

    udp receive and file replay scripts

---
+++ LirenaCapture/scripts/receiveUdpMpegTs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+## UDP RECEIVER :
+# works
+# udp -> rtpdepay -> dump to disk
+#     -> tsdemux  -> h264 dec -> scale -> show
+#                 -> klv      -> fakesink (dump klv to console)
+#                             -> dump to disk
+GST_DEBUG=0 \
+__GL_SYNC_TO_VBLANK=0 \
+gst-launch-1.0 \
+  udpsrc port=5001  \
+      caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)MP2T' ! \
+  rtpjitterbuffer latency=50 ! \
+  rtpmp2tdepay ! \
+  tee name=fork_mp2ts_to_disk_and_show \
+  \
+  fork_mp2ts_to_disk_and_show. ! \
+  queue !   \
+  filesink location=./remoteOutput_010.mpg \
+  \
+  fork_mp2ts_to_disk_and_show. ! \
+  queue !   \
+  tsdemux name=myTsDemux  \
+  \
+  myTsDemux. ! \
+    video/x-h264 ! \
+  h264parse ! \
+  queue !    \
+  avdec_h264 !   \
+  videoconvert !   \
+  videoscale !   \
+    video/x-raw,width=512,height=512 ! \
+  xvimagesink sync=false async=true \
+  \
+  myTsDemux. ! \
+    meta/x-klv,parsed=true ! \
+    queue ! \
+    tee name=myT2 \
+    \
+    myT2. ! \
+      queue ! \
+      fakesink dump=true sync=true async=true \
+    \
+    myT2. ! \
+      queue ! \
+    filesink location=fromUdp_remoteOutput_010.klv sync=true async=true
+    
+    
+exit 0
+
