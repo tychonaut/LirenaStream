8427bcd00cb3bf7e9da977e8380c2157e381fd3a
Markus Schlueter <mschlueter@geomar.de>
2021-04-17 18:16:46 +0200

    add start script, calling app  with some example params

---
+++ LirenaCapture/scripts/sendUdpMpegTs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+
+ninja -C builddir  && \
+\
+GST_DEBUG=2,appsrc:4,mpegtsmux:0,tcpserversink:3  \
+__GL_SYNC_TO_VBLANK=0 \
+./builddir/lirenaCapture \
+    192.168.1.101 5001 \
+    --exposure=45  \
+    --output=localOutput_20.mpg \
+    $@ \
+    
+    
+exit 0
+
+
+    --localdisplay \
