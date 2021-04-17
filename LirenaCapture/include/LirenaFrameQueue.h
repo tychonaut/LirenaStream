37d0acb9cc832a6e7f5d9baa8849e5e78cc32ccd
Markus Schlueter <mschlueter@geomar.de>
2021-04-17 18:37:39 +0200

    Add Thread safe queue for later decoupling of ximea acquisition and cuda processing

---
+++ LirenaCapture/include/LirenaFrameQueue.h
@@ -0,0 +1,54 @@
+#ifndef __LIRENA_FRAME_QUEUE__
+#define __LIRENA_FRAME_QUEUE__
+
+
+//https://stackoverflow.com/questions/15278343/c11-thread-safe-queue
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+// A threadsafe-queue.
+template <class T>
+class SafeQueue
+{
+public:
+  SafeQueue(void)
+    : q()
+    , m()
+    , c()
+  {}
+
+  ~SafeQueue(void)
+  {}
+
+  // Add an element to the queue.
+  void enqueue(T t)
+  {
+    std::lock_guard<std::mutex> lock(m);
+    q.push(t);
+    c.notify_one();
+  }
+
+  // Get the "front"-element.
+  // If the queue is empty, wait till a element is avaiable.
+  T dequeue(void)
+  {
+    std::unique_lock<std::mutex> lock(m);
+    while(q.empty())
+    {
+      // release lock as long as the wait and reaquire it afterwards.
+      c.wait(lock);
+    }
+    T val = q.front();
+    q.pop();
+    return val;
+  }
+
+private:
+  std::queue<T> q;
+  mutable std::mutex m;
+  std::condition_variable c;
+};
+
+#endif // __LIRENA_FRAME_QUEUE__
